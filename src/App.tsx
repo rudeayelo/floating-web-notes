14e9babdd480d370e88329a00744f239aa7d8c49
Rude <hi@rude.im>
2025-07-15 09:36:11 +0200

    refactor(App): remove unused transition state logic and adjust CSS transition timing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { Help } from "./components/Help";
 import { Notes } from "./components/Notes";
 import styles from "./styles.css?inline";
@@ -8,23 +7,14 @@ import { render } from "./utils/render";
 export const App = () => {
   const { active } = useActive();
   const { activeView } = useActiveView();
-  const [isTransitioning, setIsTransitioning] = useState(false);
   const { screenshotMode } = useScreenshotMode();
-
-  useEffect(() => {
-    if (active) {
-      setIsTransitioning(true);
-      return;
-    }
-    setIsTransitioning(false);
-  }, [active]);
 
   return active ? (
     <>
       <style type="text/css">{styles}</style>
 
       <div id="root" {...(screenshotMode && { "data-screenshot-mode": true })}>
-        <div className="Container" data-transition-state={isTransitioning}>
+        <div className="Container">
           {activeView === "help" ? <Help /> : <Notes />}
         </div>
       </div>
