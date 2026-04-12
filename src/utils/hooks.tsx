8392fd8df97539c4ce91404f7f976f404a999d8d
Rude <hi@rude.im>
2026-04-12 16:04:06 +0200

    fix: correct body tag in index.html and update isDevEnv logic in useEnv hook

--- src/utils/hooks.tsx
+++ src/utils/hooks.tsx
@@ -1,5 +1,5 @@
 export const useEnv = () => {
-  const isDevEnv = import.meta.env.DEV;
+  const isDevEnv = import.meta.env.DEV || import.meta.env.MODE === "screenshot";
   const isProdEnv = import.meta.env.PROD;
 
   return { isDevEnv, isProdEnv };
